4ca760ea982893806e2db22677a5835beaab5696
Cristopher <cristophernamchee12@gmail.com>
2021-08-25 14:27:23 +0700

    :beers: chore: Ping testing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import { config } from 'dotenv';
+import Discord, { Intents } from 'discord.js';
 
+/*
 import { Logger } from './utils/logger';
 import { bootstrapBot } from './bot';
 import { NSFWClassifier } from './utils/nsfw.classifier';
 import { Client } from 'faunadb';
 import { FaunaConfigurationRepository } from './repository/config';
+*/
 
 if (process.env.NODE_ENV === 'development') {
   config();
 }
 
 (async (): Promise<void> => {
+  /*
   const classifier = await NSFWClassifier.newClassifier();
   const secret = process.env.DB_SECRET;
 
@@ -25,8 +29,12 @@ if (process.env.NODE_ENV === 'development') {
     domain: process.env.DB_HOST,
   });
   const configRepository = new FaunaConfigurationRepository(dbClient);
-
-  const client = await bootstrapBot(classifier, configRepository);
+*/
+  // const client = await bootstrapBot(classifier, configRepository);
+  const client = new Discord.Client({
+    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
+  });
+  /*
   const cleanup = async (): Promise<void> => {
     client.destroy();
     await Promise.all([
@@ -55,6 +63,14 @@ if (process.env.NODE_ENV === 'development') {
   process.on('SIGINT', async () => {
     await cleanup();
     process.exit(0);
+  });
+
+  */
+
+  client.on('messageCreate', async (msg) => {
+    if (!msg.author.bot) {
+      msg.reply(client.ws.ping.toString());
+    }
   });
 
   await client.login(process.env.DISCORD_TOKEN);
