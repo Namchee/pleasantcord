935b9bcf3d8481c054d0131d3bee496c59138f04
Cristopher <cristophernamchee12@gmail.com>
2021-08-25 08:47:10 +0700

    :construction_worker: chore(ci): Testing CI/CD flow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,11 @@ module.exports = {
       script: './dist/index.js',
       restart_delay: 1000,
       stop_exit_codes: [0],
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
     },
   ],
 };
