14af7c3b05cc8180603fb1a37cb484bfdbbea350
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-11-30 18:48:23 +0700

    chore: use single instance for process management

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: './dist/index.js',
       restart_delay: 1000,
       stop_exit_codes: [0],
-      instances: -1,
+      instances: 1,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
