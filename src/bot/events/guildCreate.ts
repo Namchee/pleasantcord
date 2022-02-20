28d8cf23de7bbb1f8890593c0d4634647aebabc3
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-02-20 21:08:16 +0700

    feat: send error message to default channel

--- src/bot/events/guildCreate.ts
+++ src/bot/events/guildCreate.ts
@@ -1,18 +1,32 @@
-import { Guild } from 'discord.js';
+import { Guild, MessageEmbed, TextChannel } from 'discord.js';
 
 import { BASE_CONFIG } from './../../entity/config';
 import { handleError } from './../utils';
 
 import type { BotContext } from './../types';
+import { Credentials, registerSlashCommands } from '../service/command';
 
 export default {
   event: 'guildCreate',
-  fn: async ({ service }: BotContext, guild: Guild): Promise<void> => {
+  fn: async ({ client, service }: BotContext, guild: Guild): Promise<void> => {
     try {
       // create guild config when the bot enters a guild.
       await service.createConfig(guild.id, BASE_CONFIG);
+      // register slash commands for the new guild
+      const creds: Credentials = {
+        clientID: client.application?.id as string,
+        token: client.token as string,
+      };
+
+      return registerSlashCommands(guild.id, creds);
     } catch (err) {
-      handleError(err as Error);
+      const embed = handleError(err as Error) as MessageEmbed;
+
+      const defaultChannel = guild.channels.cache.find(
+        chan => chan.name.toLowerCase() === 'general'
+      ) as TextChannel;
+
+      await defaultChannel.send({ embeds: [embed] });
     }
   },
 };
